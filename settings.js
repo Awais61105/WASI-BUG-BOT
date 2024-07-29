701e1cb65da3b84d09f99abf639ac8cf04cadb55
Awais61105 <116716708+Awais61105@users.noreply.github.com>
2024-07-29 20:26:51 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,19 +3,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "923121964495"
+global.ownername = "🐛 AWAiS KiNG "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
-global.botname = 'WASI TECH BUG BOT ' //name of the bot
+global.ownernumber = '923121964495'  //creator number
+global.ownername = ' AWAiS KiNG ' //owner name
+global.botname = 'AWAiS KiNG BUG BOT ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'WASI'
+global.author = 'AWAiS'
 
 //console view/theme
 global.themeemoji = '🪀'
